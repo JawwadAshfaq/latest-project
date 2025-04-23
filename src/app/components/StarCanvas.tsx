6022dd4fa9858f5d619528955dc4c485b1f74f24
Jawwad Ashfaq <jawwadritz3@gmail.com>
2025-04-23 22:40:05 +0500

    Updated StarCanvas component

--- src/app/components/StarCanvas.tsx
+++ src/app/components/StarCanvas.tsx
@@ -36,7 +36,7 @@ const StarCanvas = () => {
     }
 
     const stars: Star[] = [];
-    const numStars = 1000;
+    const numStars = 1300;
     const numLargeStars = 8;
 
     for (let i = 0; i < numStars; i++) {
