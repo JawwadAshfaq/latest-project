6e2de408c38b845561f4d552c02516ab7ae91682
Jawwad Ashfaq <jawwadritz3@gmail.com>
2025-04-27 01:23:47 +0500

    Updated Skills section for mobile view and modified StarCanvas

--- src/app/components/StarCanvas.tsx
+++ src/app/components/StarCanvas.tsx
@@ -7,9 +7,6 @@ interface Star {
   y: number;
   size: number;
   speed: number;
-  isLarge: boolean;
-  dx: number;
-  dy: number;
 }
 
 const StarCanvas = () => {
@@ -37,21 +34,13 @@ const StarCanvas = () => {
 
     const stars: Star[] = [];
     const numStars = 1100;
-    const numLargeStars = 8;
 
     for (let i = 0; i < numStars; i++) {
-      const isLarge = i < numLargeStars;
-      const dx = Math.random() * 0.5 - 0.25;
-      const dy = Math.random() * 0.5 - 0.25;
-
       stars.push({
         x: Math.random() * canvas.width,
         y: Math.random() * canvas.height,
-        size: isLarge ? Math.random() * 2 + 1 : Math.random() * 0.5 + 0.2,
-        speed: isLarge ? Math.random() * 4.5 + 4.5 : Math.random() * 0.5 + 0.3,
-        isLarge,
-        dx,
-        dy,
+        size: Math.random() * 0.5 + 0.2,
+        speed: Math.random() * 0.5 + 0.3,
       });
     }
 
@@ -65,19 +54,11 @@ const StarCanvas = () => {
         ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
         ctx.fill();
 
-        if (star.isLarge) {
-          star.x += star.dx * star.speed;
-          star.y += star.dy * star.speed;
-          if (star.x < 0) star.x = canvas.width;
-          if (star.x > canvas.width) star.x = 0;
-          if (star.y < 0) star.y = canvas.height;
-          if (star.y > canvas.height) star.y = 0;
-        } else {
-          star.x += star.speed;
-          star.y += star.speed;
-          if (star.x > canvas.width) star.x = 0;
-          if (star.y > canvas.height) star.y = 0;
-        }
+        star.x += star.speed;
+        star.y += star.speed;
+
+        if (star.x > canvas.width) star.x = 0;
+        if (star.y > canvas.height) star.y = 0;
       }
 
       requestAnimationFrame(animate);
